53daddf8bb053de3cf15c8fc7cd6fc607693707f
Mikołaj Radek <radek_mikolaj@o2.pl>
2022-06-22 00:14:08 +0200

    moblie nav_bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,51 @@
 import React from 'react';
+import {
+    Link,
+} from 'react-router-dom'
+import './NavBar.scss'
 
 function NavBar() {
     return (
-        <div>xd</div>
+        <section className="navigation">
+            <div className="fb_box">
+                <h1
+                    className="main_text"
+                >
+                    facebook
+                </h1>
+                <div className="icon_box">
+                    <div className="search_box">
+                        <div className="search"/>
+                    </div>
+                    <div className="messenger_box">
+                        <div className="messenger"/>
+                    </div>
+                </div>
+            </div>
+            <div
+                className="menu_box"
+            >
+                <div
+                    className="home"
+                />
+                <div
+                    className="shop"
+                />
+                <div
+                    className="user"
+                />
+                <div
+                    className="watch"
+                />
+                <div
+                    className="bell"
+                />
+                <Link
+                    className="burger"
+                    to="/Menu"
+                />
+            </div>
+        </section>
     );
 }
 
