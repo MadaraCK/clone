5a36535bb3853e4f14e7ff955a3f148427eebf0e
Mikołaj Radek <radek_mikolaj@o2.pl>
2022-06-22 00:37:24 +0200

    menu

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import {
     Routes,
 } from 'react-router-dom';
 import NavBar from "./components/NavBar/NavBar";
+import Menu from "./components/NavBar/Menu/Menu";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<NavBar/>} path="/"/>
+                <Route element={<Menu/>} path="/Menu"/>
             </Routes>
         </BrowserRouter>
     );
--- src/components/NavBar/Menu/Menu.js
+++ src/components/NavBar/Menu/Menu.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import '../NavBar.scss'
 
 function Menu() {
     return (
-        <div>menu</div>
+        <div
+            className="menu_box"
+        >
+            <div
+                className="home"
+            />
+            <div
+                className="shop"
+            />
+            <div
+                className="user"
+            />
+            <div
+                className="watch"
+            />
+            <div
+                className="bell"
+            />
+        </div>
     );
 }
 
